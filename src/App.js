debea4d15bb005e5dc4a32f7a99c80bceb583422
Osama Ayman <89817638+osama-ayman77@users.noreply.github.com>
2024-08-22 15:50:59 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { useState } from "react";
 const initialFriends = [
   {
     id: 118836,
-    name: "Clark",
+    name: "Moataz",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
   },
   {
     id: 933372,
-    name: "Sarah",
+    name: "Ziad",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
     id: 499476,
-    name: "Anthony",
+    name: "Hossam",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
